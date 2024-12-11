8978ea7d2f380468ede50fabd734b85d12568abe
Sashakrm <sasakramch@gmail.com>
2024-12-11 08:55:12 +0200

    Add files via upload

---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int day = Input("day= ");
+            string dayWeek = GetDay(day);
+            Console.WriteLine($"{dayWeek}");
+            Console.Read();
+        }
+        static int Input(string message)
+        {
+            Console.Write(message);
+            return int.Parse(Console.ReadLine());
+        }
+        static string GetDay(int day)
+        {
+            switch(day)
+            {
+                case 1: return "Понедельник";
+                case 2: return "Вторник";
+                case 3: return "Среда";
+                case 4: return "Четверг";
+                case 5: return "Пятница";
+                case 6: return "Суббота";
+                case 7: return "Воскресенье";
+                default: return "Ошибка";
+            }
+        }
+    }
+}
