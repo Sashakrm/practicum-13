a848211725c0fa9a0ae52f1e7c5d52c214c72ddf
Sashakrm <sasakramch@gmail.com>
2024-12-11 09:29:15 +0200

    Add files via upload

---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int month = Input("Месяц по счету: ");
+            string mon = Month(month);
+            Console.Write($"{mon}");
+            Console.Read();
+        }
+        static int Input(string mes)
+        {
+            Console.Write(mes);
+            return int.Parse(Console.ReadLine());
+        }
+        static string Month(int month)
+        {
+            switch (month)
+            {
+                case 1: return "Январь";
+                case 2: return "Февраль";
+                case 3: return "Март";
+                case 4: return "Апрель";
+                case 5: return "Май";
+                case 6: return "Июнь";
+                case 7: return "Июль";
+                case 8: return "Август";
+                case 9: return "Сентябрь";
+                case 10: return "Октябрь";
+                case 11: return "Ноябрь";
+                case 12: return "Декабрь";
+                default: return "Ошибка";
+            }
+        }
+    }
+}
